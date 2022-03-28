0da6ff720d4624a70fe6de339bae769b85716e93
Augusto <augustoamaro@icloud.com>
2022-03-28 00:02:14 -0300

    set abilities to pokemon

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Modal.css";
 
 const Modal = ({ pokemon, closeModal }) => {
+    const [abilities, setAbilities] = useState([]);
+    // const [sprites, setSprites] = useState([]);
+
+    useEffect(() => {
+        fetch(pokemon.url)
+            .then(res => res.json())
+            // .then(data => console.log(data))
+            .then(data => {
+                setAbilities(data.abilities)
+                // setSprites(data.sprites)
+            })
+    })
 
     return (
 
@@ -9,15 +21,25 @@ const Modal = ({ pokemon, closeModal }) => {
             <div className="modal">
                 < div className="overlay" ></div >
                 <div className="modal-content">
-                    <h2>Hello Modal</h2>
-                    <p>
+
+                    {/* {sprites.map((sprite) => {
+                        return console.log(sprite)
+                        return <img>{sprite.front_default}</img>
+                    })} */}
+
+
+                    <p className="pokemon">
                         {pokemon.name}
                     </p>
-                    <p>
-                        {pokemon.url}
-                    </p>
+
+
+                    {abilities.map((ability, index) => {
+                        return <p key={index}>Skill: {ability.ability.name}</p>
+                    })}
+
+
                     <button onClick={closeModal} className="close-modal">
-                        CLOSE
+                        X
                     </button>
                 </div>
             </div > : <></>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import Card from "../components/Card";
 import Modal from "../components/Modal";
 
 import { useEffect, useState } from "react";
